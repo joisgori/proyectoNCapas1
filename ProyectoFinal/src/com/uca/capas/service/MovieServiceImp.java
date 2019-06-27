7ab84bdb22e97c2330f7d3eb382c0ff511062558
joisgori <00034715@uca.edu.sv>
2019-06-27 16:40:53 -0600

    Implemento el transactional de hibernate

--- ProyectoFinal/src/com/uca/capas/service/MovieServiceImp.java
+++ ProyectoFinal/src/com/uca/capas/service/MovieServiceImp.java
@@ -2,6 +2,8 @@ package com.uca.capas.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
